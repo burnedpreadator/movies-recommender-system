7ca3e986be403d47a4ab38491e1e49073f7301b5
burnedpredator <chhariavikram1@gmail.com>
2021-08-31 21:04:48 +0530

    input recieved and data fetched

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from "react";
-import { Switch,Route } from 'react-router-dom'
+import { Switch,Route } from 'react-router-dom';
 
-import Navs from "./components/Navs";
 import Home from "./pages/Home";
 import Starred from "./pages/Starred";
 
 function App() {
-  return <div>
-
-      <Navs />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/starred">
-          <Starred />
-        </Route>
-        <Route>
-            <div>Not Found</div>
-        </Route>
-      </Switch>
-    </div>
-}
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
+      <Route>
+          <div>Not Found</div>
+      </Route>
+    </Switch>
+  )
+};
 
 export default App;
